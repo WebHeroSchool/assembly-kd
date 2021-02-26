0992af41f69bac05829475717450f385214c327a
IlyaKd <ilyakd@protonmail.com>
2021-02-26 13:40:19 +0300

    added watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,47 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cssnano = require('gulp-cssnano');
-var sourcemaps = require('gulp-sourcemaps');
+const sourcemaps = require('gulp-sourcemaps');
+
+const paths = {
+    src: {
+        styles: 'src/styles/*.css',
+        scripts: 'src/scripts/*.js'
+    },
+    build: {
+        styles: 'build/styles',
+        scripts: 'build/scripts'
+    },
+    buildNames: {
+        styles: 'index.min.css',
+        scripts: 'index.min.js'
+    }
+}
 
 gulp.task('build-css', () => {
-    return gulp.src(['css/*.css'])
+    return gulp.src([paths.src.styles])
         .pipe(sourcemaps.init())
-            .pipe(contact('index.css'))
+            .pipe(contact(paths.buildNames.styles))
             .pipe(cssnano())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/css'));
+        .pipe(gulp.dest(paths.build.styles));
 });
 
 gulp.task('build-js', () => {
-    return gulp.src(['scripts/*.js'])
+    return gulp.src([paths.src.scripts])
         .pipe(sourcemaps.init())
-            .pipe(concat('index.js'))
+            .pipe(concat(paths.buildNames.scripts))
             .pipe(babel({
                 presets: ['@babel/env']
             }))
             .pipe(uglify())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/js'));
+        .pipe(gulp.dest(paths.build.scripts));
 });
 
 gulp.task('build', ['build-css', 'build-js']);
+
+gulp.task('watch', () => {
+    gulp.watch(paths.src.styles, ['build-css']);
+    gulp.watch(paths.src.scripts, ['build-js']);
+})
