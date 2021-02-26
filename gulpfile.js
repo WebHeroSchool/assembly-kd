483183ba59d6743b115de733bb9e0497f4c3cb4f
IlyaKd <ilyakd@protonmail.com>
2021-02-26 12:36:45 +0300

    install sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,26 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cssnano = require('gulp-cssnano');
-
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('build-css', () => {
     return gulp.src(['css/*.css'])
-        .pipe(contact('index.css'))
-        .pipe(cssnano())
+        .pipe(sourcemaps.init())
+            .pipe(contact('index.css'))
+            .pipe(cssnano())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('build-js', () => {
     return gulp.src(['scripts/*.js'])
-        .pipe(concat('index.js'))
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(uglify())
+        .pipe(sourcemaps.init())
+            .pipe(concat('index.js'))
+            .pipe(babel({
+                presets: ['@babel/env']
+            }))
+            .pipe(uglify())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('build/js'));
 });
 
